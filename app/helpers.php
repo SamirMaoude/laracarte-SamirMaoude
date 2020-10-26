37579cfa6d1acb5fd93c25510b8cd0b2ba14fab9
SamirMaoude <samirmaoude7@gmail.com>
2020-10-27 00:23:16 +0100

    Add set_active_route helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 
 if(! function_exists('page_title'))
 {
     function page_title($title)
     {
         return $title.' | Laracarte - List of artisans';
+    }
+}
+
+if(! function_exists('set_active_route'))
+{
+    function set_active_route($route)
+    {
+        return Route::is($route) ? 'active' : '';
     }
 }
